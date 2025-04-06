067ab9123f96757ec425acfe88097fed1f60bf2e
Sambhav <sambhav.jain@apnibus.com>
2025-04-07 00:48:50 +0530

    Add build.sh for Render deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # build.sh
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Applying migrations..."
 python manage.py migrate
